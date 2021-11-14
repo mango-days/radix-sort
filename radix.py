ba6755fbeb173caab493a3350a652fe8586b2a98
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-15 00:38:30 +0530

    Update radix.py

--- radix.py
+++ radix.py
@@ -3,13 +3,13 @@ array = [170, 45, 75, 90, 802, 24, 2, 66]
 def countingSort ( array, exp ) :
     output = [0] * len(array)
     count = [0] * (10)
- 
+    
     for i in range ( 0, len(array) ) :
         index = int (array[i] / exp)
         count[ index % 10 ] += 1
- 
+        
     for i in range ( 1, 10 ) : count[i] += count[i - 1]
- 
+    
     i = len(array) - 1
     while i >= 0:
         index = int (array[i] / exp)
